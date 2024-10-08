1861f364bd52b624caf452dad1ba4443baab2a12
Ahmadghazal1 <74017377+Ahmadghazal1@users.noreply.github.com>
2024-10-08 23:08:47 +0300

    Add toaster for delete a card reader

--- API/ProgressSoft.API/obj/Debug/net8.0/ProgressSoft.API.AssemblyInfo.cs
+++ API/ProgressSoft.API/obj/Debug/net8.0/ProgressSoft.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgressSoft.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17cb20ef6bc69aeb77c2f6359a4114413212acb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d189409a72626e2dd6eff43d6c511ee2b9110a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgressSoft.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgressSoft.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
